44a6f8a2c4750e91364068fa74b2ffacb366722f
Alvaro <alvaro99.cunha@gmail.com>
2022-10-14 14:38:17 -0300

    added unhappy path car test

--- tests/models/testCar.py
+++ tests/models/testCar.py
@@ -1,17 +1,26 @@
 import unittest
+
 from src.models.Car import Car
 
 class CarTest(unittest.TestCase):
+
+  # cada metodo de teste tem que ter o prefixo 'test_'
+
+  def setUp(self):
+    self.model = 'Ford Focus'
+    self.year = '2012/2013'
+    self.color = 'Silver'
+
   def test_valid_car(self):
-    model = 'Ford Focus'
-    year = '2012/2013'
-    color = 'Silver'
+    car = car = Car(model=self.model, year=self.year, color=self.color)
 
-    car = car = Car(model=model, year=year, color=color)
-
-    self.assertEqual(car.get_model(), model)
-    self.assertEqual(car.get_year(), year)
-    self.assertEqual(car.get_color(), color)
+    self.assertEqual(car.get_model(), self.model)
+    self.assertEqual(car.get_year(), self.year)
+    self.assertEqual(car.get_color(), self.color)
     
     self.assertTrue(car.get_is_clean())
     self.assertFalse(car.get_is_engine_running())
+
+  @unittest.expectedFailure
+  def test_invalid_car(self):
+    Car(model=self.model, year=self.year)
